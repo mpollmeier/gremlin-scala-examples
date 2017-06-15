3eaf07c5381bcdd8ff88bbaad4c973af5825f739
Michael Pollmeier <michael@michaelpollmeier.com>
2017-06-15 11:50:15 +0200

    fix neo4j index spec

--- neo4j/src/test/scala/IndexSpec.scala
+++ neo4j/src/test/scala/IndexSpec.scala
@@ -2,53 +2,51 @@ import gremlin.scala._
 import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph
 import org.apache.tinkerpop.gremlin.process.traversal.Path
 import org.neo4j.graphdb.DynamicLabel
+import org.neo4j.tinkerpop.api.impl.Neo4jGraphAPIImpl
 import scala.util.Random
 import collection.JavaConversions._
 import org.scalatest._
 
-// indices have to be set directly in neo4j nowadays...
-// http://tinkerpop.incubator.apache.org/docs/3.0.0-incubating/#_indices
-
-// inspired by https://github.com/tinkerpop/tinkerpop3/issues/359#event-197211058
+// http://tinkerpop.apache.org/docs/current/reference/#neo4j-gremlin
 class IndexSpec extends FlatSpec with Matchers {
-  "Gremlin-Scala" should "create some vertices with properties" ignore {
-    // val dbPath = "target/indexspec"
-    // FileUtils.removeAll(dbPath)
-    // val graph: Neo4jGraph = Neo4jGraph.open(dbPath)
-    // val gs = GremlinScala(graph)
+  "Gremlin-Scala" should "create some vertices with indexed properties" in {
+    val dbPath = "target/indexspec"
+    FileUtils.removeAll(dbPath)
+    val graph = Neo4jGraph.open(dbPath)
+    val scalaGraph = graph.asScala
+    val db = graph.getBaseGraph.asInstanceOf[Neo4jGraphAPIImpl].getGraphDatabase
 
-    // graph.tx.open
-    // val l = DynamicLabel.label("Person")
-    // val s = graph.getBaseGraph.schema
-    // s.indexFor(l).on("name").create
-    // graph.tx.close
+    graph.tx.open
+    val label = DynamicLabel.label("Person")
+    db.schema.indexFor(label).on("name").create
+    graph.tx.commit
+    graph.tx.close
 
-//     val vertexCount = 10000
-//     (1 to vertexCount) foreach { i ⇒
-//       val name = i.toString
-//       val nonIndexedString = i.toString
-//       val vertex = s.addVertex(label = "Person")
-//       vertex.setProperty("name", name)
-//       vertex.setProperty("nonIndexedString", nonIndexedString)
-//     }
+    val vertexCount = 10000
+    graph.tx.open
+    (1 to vertexCount) foreach { i ⇒
+      scalaGraph.addVertex(label = "Person", Map("name" -> i.toString, "nonIndexedString" -> i.toString))
+    }
+    graph.tx.commit
+    graph.tx.close
 
-//     def timeLookups(propertyName: String): Long = {
-//       val t0 = System.currentTimeMillis
-//       (1 to 100).foreach { _ ⇒
-//         val i = Random.nextInt(vertexCount)
-//         val v = gs.V.has(T.label, "Person").has(propertyName, i.toString).headOption
-//         assert(v.isDefined)
-//       }
-//       val t1 = System.currentTimeMillis
-//       t1 - t0
-//     }
+    def timeLookups(propertyName: String): Long = {
+      val t0 = System.currentTimeMillis
+      (1 to 100).foreach { _ ⇒
+        val i = Random.nextInt(vertexCount)
+        val v = scalaGraph.V.hasLabel("Person").has(Key[String](propertyName), i.toString).headOption
+        assert(v.isDefined)
+      }
+      val t1 = System.currentTimeMillis
+      t1 - t0
+    }
 
-//     val timeNonIndexed = timeLookups("nonIndexedString")
-//     val timeIndexed = timeLookups("name")
-//     println(s"time for lookups of non-indexed vertices: ${timeNonIndexed}ms")
-//     println(s"time for lookups of indexed vertices: ${timeIndexed}ms")
-//     timeIndexed should be < timeNonIndexed
+    val timeNonIndexed = timeLookups("nonIndexedString")
+    val timeIndexed = timeLookups("name")
+    println(s"time for lookups of non-indexed vertices: ${timeNonIndexed}ms")
+    println(s"time for lookups of indexed vertices: ${timeIndexed}ms")
+    timeIndexed should be < timeNonIndexed
 
-    // graph.close
+    graph.close
   }
 }
