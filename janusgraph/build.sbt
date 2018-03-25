f7c79ac55d5648870b00f69ca2cfdce05bc8f399
Michael Pollmeier <michael@michaelpollmeier.com>
2018-03-25 21:00:23 +1300

    upgrade janusgraph example

--- janusgraph/build.sbt
+++ janusgraph/build.sbt
@@ -3,7 +3,8 @@ organization := "com.michaelpollmeier"
 scalaVersion := "2.12.4"
 
 libraryDependencies ++= Seq(
-  "com.michaelpollmeier" %% "gremlin-scala" % "3.3.0.5",
+  "com.michaelpollmeier" %% "gremlin-scala" % "3.3.1.2-SNAPSHOT",
+  "org.apache.tinkerpop" % "gremlin-driver" % "3.3.1",
   "org.janusgraph" % "janusgraph-core" % "0.2.0",
   "ch.qos.logback" % "logback-classic" % "1.2.3" % Test,
   "org.scalatest" %% "scalatest" % "3.0.3" % Test
--- janusgraph/src/test/scala/SimpleSpec.scala
+++ janusgraph/src/test/scala/SimpleSpec.scala
@@ -6,34 +6,36 @@ import org.apache.commons.configuration.BaseConfiguration
 
 class SimpleSpec extends WordSpec with Matchers {
 
-  "connect to janusgraph, pull out Saturn's keys and shutdown cleanly" in {
-    val conf = new BaseConfiguration()
-    conf.setProperty("storage.backend","inmemory")
-    val graph = JanusGraphFactory.open(conf)
-    val scalaGraph = graph.asScala
+  "connect to janusgraph, pull out Saturn's keys and shutdown cleanly" when {
+    "using inmemory janusgraph instance" in {
+      val conf = new BaseConfiguration()
+      conf.setProperty("storage.backend","inmemory")
+      val graph = JanusGraphFactory.open(conf)
+      implicit val scalaGraph = graph.asScala
 
-    val Name = Key[String]("name")
-    val Planet = "planet"
-    val Saturn = "saturn"
+      val Name = Key[String]("name")
+      val Planet = "planet"
+      val Saturn = "saturn"
 
-    (1 to 4) foreach { i ⇒
-      graph + (Planet, Name -> s"vertex $i")
+      (1 to 4) foreach { i ⇒
+        graph + (Planet, Name -> s"vertex $i")
+      }
+      val saturnV = graph + (Saturn, Name -> Saturn)
+      val sunV = graph + ("sun", Name -> "sun")
+      saturnV --- "orbits" --> sunV
+
+      graph.V.count.head shouldBe 6
+      graph.E.count.head shouldBe 1
+
+      val traversal = graph.V.value(Name)
+      traversal.toList.size shouldBe 6
+
+      graph.V.hasLabel(Saturn).count.head shouldBe 1
+
+      val saturnQ = graph.V.hasLabel(Saturn).head
+      saturnQ.value2(Name) shouldBe Saturn
+
+      graph.close
     }
-    val saturnV = graph + (Saturn, Name -> Saturn)
-    val sunV = graph + ("sun", Name -> "sun")
-    saturnV --- "orbits" --> sunV
-
-    graph.V.count.head shouldBe 6
-    graph.E.count.head shouldBe 1
-
-    val traversal = graph.V.value(Name)
-    traversal.toList.size shouldBe 6
-
-    graph.V.hasLabel(Saturn).count.head shouldBe 1
-
-    val saturnQ = graph.V.hasLabel(Saturn).head
-    saturnQ.value2(Name) shouldBe Saturn
-
-    graph.close
   }
 }
